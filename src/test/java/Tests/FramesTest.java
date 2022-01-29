324f33c5bf2722d64e26e12bfea62b53667bfbec
radubot10 <radu.botareanu@gmail.com>
2022-01-29 14:03:59 +0200

    Test Frames Finalizat

---
+++ src/test/java/Tests/FramesTest.java
@@ -0,0 +1,45 @@
+package Tests;
+
+import Base.SharedData;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class FramesTest extends SharedData {
+
+    @Test
+    public void framesTest(){
+
+        WebElement skipSignInElement = driver.findElement(By.id("btn2"));
+        skipSignInElement.click();
+
+        WebElement switchToElement = driver.findElement(By.xpath("//a[text()='SwitchTo']"));
+        Actions Action = new Actions(driver);
+        Action.moveToElement(switchToElement).perform();
+
+        WebElement framesElement = driver.findElement(By.xpath("//a[text()='Frames']"));
+        framesElement.click();
+
+        driver.navigate().to("http://demo.automationtesting.in/Frames.html");
+
+        driver.switchTo().frame("singleframe");
+        WebElement inputText = driver.findElement(By.cssSelector("div>input[type='text']"));
+        inputText.sendKeys("Foame");
+
+        driver.switchTo().defaultContent();
+
+        WebElement multipleIframe = driver.findElement(By.cssSelector("a[href='#Multiple']"));
+        multipleIframe.click();
+
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[src='MultipleFrames.html']")));
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[src='SingleFrame.html']")));
+        WebElement inputText2 = driver.findElement(By.cssSelector("div>input[type='text']"));
+        inputText2.sendKeys("Imi e si mai foame acum.");
+
+    }
+
+
+
+
+}
